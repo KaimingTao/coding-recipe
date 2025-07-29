91141ce60c6a2af70493326d4c69cacbcc00c0ed
Kaiming <mads396@gmail.com>
2025-07-29 11:08:46 -0700

    w

--- bioinfo/position_offset/position_convert.py
+++ bioinfo/position_offset/position_convert.py
@@ -1,11 +1,18 @@
-def aa_pos_to_na_pos(aa_pos):
+def aa_pos_to_codon_start(aa_pos):
     aa_ofst = aa_pos - 1
     na_ofst = aa_ofst * 3
-    return (
-        na_ofst * 3 + 1,
-        na_ofst * 3 + 2,
-        na_ofst * 3 + 3
+
+    return na_ofst * 3 + 1
+
+
+def aa_pos_to_codon_range(aa_pos):
+    aa_ofst = aa_pos - 1
+    na_ofst = aa_ofst * 3
+    return range(
+        na_ofst + 1,
+        na_ofst + 4
     )
+
 
 def na_pos_to_aa_pos(na_pos):
 
@@ -13,3 +20,16 @@ def na_pos_to_aa_pos(na_pos):
     aa_ofst = na_ofst // 3
 
     return aa_ofst + 1
+
+
+def na_pos_to_codon_start(na_pos):
+    pass
+
+
+def na_pos_to_codon_range(na_pos):
+    aa_pos = na_pos_to_aa_pos(na_pos)
+    return aa_pos_to_codon_range(aa_pos)
+
+
+def test():
+    
