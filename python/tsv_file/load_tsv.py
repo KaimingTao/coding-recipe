a4c148b4fa9662d0e9e52b19e8afccb0b3aaa9c5
Kaiming <mads396@gmail.com>
2025-07-29 15:51:52 -0700

    w

--- python/tsv_file/load_tsv.py
+++ python/tsv_file/load_tsv.py
@@ -1,13 +1,13 @@
 import csv
 from pathlib import Path
 
-def load_csv(file_path, encoding='utf-8-sig'):
+def load_tsv(file_path, encoding='utf-8-sig'):
     file_path = Path(file_path).resolve()
 
     table = []
 
     with open(file_path, encoding=encoding) as fd:
-        for record in csv.DictReader(fd):
+        for record in csv.DictReader(fd, delimiter='\t'):
             table.append(record)
 
     return table
