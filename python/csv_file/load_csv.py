e20ab649999c42de9476818f107aca202ad3767f
Kaiming <mads396@gmail.com>
2025-08-05 10:23:04 -0700

    fix load csv

--- python/csv_file/load_csv.py
+++ python/csv_file/load_csv.py
@@ -7,7 +7,7 @@ def load_csv(file_path, encoding='utf-8-sig'):
     table = []
 
     with open(file_path, encoding=encoding) as fd:
-        for record in csv.DictReader(fd, delimiter='\t'):
+        for record in csv.DictReader(fd, delimiter=','):
             table.append(record)
 
     return table
