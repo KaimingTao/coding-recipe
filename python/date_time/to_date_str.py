c3937bd5eb0b95a907aab123be313fa5eeeec13b
Kaiming <mads396@gmail.com>
2025-08-05 10:47:01 -0700

    w

---
+++ python/date_time/to_date_str.py
@@ -0,0 +1,11 @@
+
+def to_date_str(date_obj, format='%Y-%m-%d'):
+    return date_obj.strftime(format)
+
+
+def to_time_str(time_obj, format='%H:%M:%S'):
+    return time_obj.strftime(format)
+
+
+def to_datetime_str(datetime_obj):
+    pass
